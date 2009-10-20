4cdb5ac760e508732d06a1702d5329fb34146ba6
Akira Sawada <aklaswad@gmail.com>
2009-10-21 00:56:00 +0900

    Removed infinite loop by multiple override. also added a few tests.

--- plugins/TagOverride/lib/TagOverride.pm
+++ plugins/TagOverride/lib/TagOverride.pm
@@ -5,12 +5,14 @@ use warnings;
 sub _hdlr_tag_override {
     my ($ctx, $args) = @_;
     my $name = $args->{name};
-    my $super = $ctx->handler_for($name)
+    my $super = $ctx->handler_for(lc $name)
         or return $ctx->error("Cannot override for unexisting tag $name");
     my $tokens = $ctx->stash('tokens');
     my $runner = sub {
         my ( $local_ctx, $local_args, $local_cond ) = @_;
         my $hdlr = $local_ctx->stash('__handler');
+        local $local_ctx->{__stash}{tag} = lc $name;
+        local $local_ctx->{__stash}{__installed_tag} = lc $name;
         local $local_ctx->{__stash}{__installed_handler} = $hdlr;
         # keep $tokens into this closure.
         local $local_ctx->{__stash}{orig_tokens} = $local_ctx->{__stash}{tokens};
@@ -29,7 +31,7 @@ sub _hdlr_super_tag {
     my ( $ctx, $args, $cond ) = @_;
     my $hdlr = $ctx->stash('__installed_handler')
         or return $ctx->error('Cannot use mt:SuperTag outside of TagOverride block');
-
+    my $tag = $ctx->stash('__installed_tag');
     ## import args from var
     my $orig_args = $ctx->var('args');
     my %orig_vars;
@@ -37,9 +39,14 @@ sub _hdlr_super_tag {
         $orig_vars{$k} = $args->{$k};
         $args->{$k} = $orig_args->{$k};
     }
-    local $ctx->{__stash}{tokens} = $ctx->{__stash}{orig_tokens};
-    my $res = $hdlr->invoke_super(@_)
-        or return $ctx->error('Failed to invoke super handler: ' . $ctx->errstr);
+    my $res;
+    {
+        local $ctx->{__stash}{tokens} = $ctx->{__stash}{orig_tokens};
+        local $ctx->{__stash}{__installed_handler} = $hdlr->super;
+        local $ctx->{__stash}{tag} = $tag;
+        $res = $hdlr->invoke_super($ctx, $args, $cond)
+            or return $ctx->error('Failed to invoke super handler: ' . $ctx->errstr);
+    }
     for my $k ( keys %orig_vars ) {
         $args->{$k} = $orig_vars{$k};
     }
---
+++ t/TagOverride-00-tags.t
@@ -0,0 +1,53 @@
+use strict;
+use warnings;
+use lib qw( lib extlib ../lib ../extlib t/lib t/extlib );
+use MT::Test qw(:db :data);
+use Test::More;
+use MT;
+use MT::Builder;
+use MT::Util::YAML;
+my $mt    = MT->new;
+my $build = MT::Builder->new;
+my $ctx   = MT::Template::Context->new;
+$ctx->stash(blog_id => 1);
+$ctx->stash(blog => MT->model('blog')->load(1));
+
+sub test {
+    my ($tmpl, $expect, $desc) = @_;
+    my $tokens = $build->compile($ctx, $tmpl);
+    my $result = $build->build($ctx, $tokens);
+    is( $result, $expect, $desc ); 
+}
+
+my $data;
+{
+    local $/;
+    $data = <DATA>;
+}
+my $test_suite = MT::Util::YAML::Load($data);
+$test_suite = $test_suite->{tests};
+plan tests => ( scalar @$test_suite );
+my $i = 0;
+for my $test ( @$test_suite ) {
+    test( $test->{tmpl}, $test->{expect}, $test->{description} || "Template " . $i++ );
+}
+
+__END__
+# 0
+tests:
+    -
+        description: install
+        tmpl: '<mt:tagOverride name="entryTitle">foo<mt:superTag>bar</mt:tagOverride>'
+        expect: ''
+    -
+        tmpl: '<mt:entries id="1"><mt:entryTitle></mt:entries>'
+        expect: fooA Rainy Daybar
+    -
+        tmpl: '<mt:tagOverride name="entryTitle">fizz<mt:superTag>buzz</mt:tagOverride>'
+        expect: ''
+        description: one more override
+    -
+        tmpl: '<mt:entries id="1"><mt:entryTitle></mt:entries>'
+        expect: 'fizzfooA Rainy Daybarbuzz'
+
+
